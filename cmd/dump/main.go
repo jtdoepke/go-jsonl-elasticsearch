a0de27592fe7ca37195cdd041e7377e3b430ae92
Jaye Doepke <jdoepke@mintel.com>
2023-03-29 11:47:03 -0500

    Size must be 1

--- cmd/dump/main.go
+++ cmd/dump/main.go
@@ -87,7 +87,7 @@ func readIndex(ctx context.Context, c chan<- *model.ESSearchResponse) error {
 		resp, err := es_client.Search(
 			es_client.Search.WithContext(ctx),
 			es_client.Search.WithBody(esutil.NewJSONReader(b)),
-			es_client.Search.WithSize(*size),
+			es_client.Search.WithSize(1),
 			es_client.Search.WithTrackTotalHits(false),
 			es_client.Search.WithIndex(*es_index),
 			es_client.Search.WithSort("_doc"),
